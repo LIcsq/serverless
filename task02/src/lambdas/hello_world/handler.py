919177df7736dbd08cce45847af31afab620f475
Uliana Neiliuk <uliana_neiliuk@epam.com>
2024-05-14 18:21:30 +0300

    change response

--- task02/src/lambdas/hello_world/handler.py
+++ task02/src/lambdas/hello_world/handler.py
@@ -1,14 +1,16 @@
+import json
+
 def lambda_handler(event, context):
     path = event['requestContext']['http']['path']
     method = event['requestContext']['http']['method']
     if path == '/hello' and method == 'GET':
         response = {
             'statusCode': 200,
-            'message': 'Hello from Lambda'
+            'body': json.dumps({'statusCode': 200, 'message': 'Hello from Lambda'})
         }
     else:
         response = {
             'statusCode': 400,
-            'message': f'Bad request syntax or unsupported method. Request path: {path}. HTTP method: {method}'
+            'body': json.dumps({'statusCode': 400, 'message': f'Bad request syntax or unsupported method. Request path: {path}. HTTP method: {method}'})
         }
     return response
