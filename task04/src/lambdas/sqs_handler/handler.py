fc1b5cfc928e853ac3a07d0a7a464fa39b90f778
Uliana Neiliuk <uliana_neiliuk@epam.com>
2024-06-13 21:34:42 +0300

    task04

--- task04/src/lambdas/sqs_handler/handler.py
+++ task04/src/lambdas/sqs_handler/handler.py
@@ -3,11 +3,10 @@
 def lambda_handler(event, context):
     for message in event['Records']:
         process_message(message)
-    print("done")
 
 def process_message(message):
     try:
-        print(f"Processed message {message['body']}")
+        print(message['body'])
     except Exception as err:
         print("An error occurred")
         raise err
