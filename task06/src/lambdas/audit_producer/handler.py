13f60c06c006bc5a4bb4a2b93a91a6331b81e800
Uliana Neiliuk <uliana_neiliuk@epam.com>
2024-07-12 22:14:50 +0300

    lambda hhandler

--- task06/src/lambdas/audit_producer/handler.py
+++ task06/src/lambdas/audit_producer/handler.py
@@ -1,24 +1,45 @@
-from commons.log_helper import get_logger
-from commons.abstract_lambda import AbstractLambda
+import datetime
+import os
+import uuid
+import boto3
 
-_LOG = get_logger('AuditProducer-handler')
-
-
-class AuditProducer(AbstractLambda):
-
-    def validate_request(self, event) -> dict:
-        pass
-        
-    def handle_request(self, event, context):
-        """
-        Explain incoming event here
-        """
-        # todo implement business logic
-        return 200
-    
-
-HANDLER = AuditProducer()
-
+dynamodb = boto3.client('dynamodb')
+audit_table = os.environ['audit_table']
 
 def lambda_handler(event, context):
-    return HANDLER.lambda_handler(event=event, context=context)
+    for record in event['Records']:
+        if record['eventName'] == 'INSERT':
+            new_image = record['dynamodb']['NewImage']
+            audit_item = {
+                'id': {'S': str(uuid.uuid4())},
+                'itemKey': {'S': new_image['key']['S']},
+                'modificationTime': {'S': datetime.datetime.utcnow().isoformat() + 'Z'},
+                'newValue': { 'M': {
+                    'key': {'S': new_image['key']['S']},
+                    'value': {'N': new_image['value']['N']}
+                    }
+                }
+            }
+            dynamodb.put_item(TableName=audit_table,
+                              Item=audit_item
+                              )
+        
+        elif record['eventName'] == 'MODIFY':
+            old_image = record['dynamodb']['OldImage']
+            new_image = record['dynamodb']['NewImage']
+            if old_image['value']['N'] != new_image['value']['N']:
+                audit_item = {
+                    'id': {'S': str(uuid.uuid4())},
+                    'itemKey': {'S': new_image['key']['S']},
+                    'modificationTime': {'S': datetime.datetime.utcnow().isoformat() + 'Z'},
+                    'updatedAttribute': {'S': 'value'},
+                    'oldValue': {'N': old_image['value']['N']},
+                    'newValue': {'N': new_image['value']['N']}
+                }
+                dynamodb.put_item(TableName=audit_table,
+                              Item=audit_item
+                              )
+    
+    return {
+        'StatusCode': 200
+        }
